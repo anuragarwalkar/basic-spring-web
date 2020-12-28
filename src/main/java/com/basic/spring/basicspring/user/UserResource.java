0d17ce4226d251ff731700be976535b33bee2897
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-28 22:20:52 +0530

    added exception handling

--- src/main/java/com/basic/spring/basicspring/user/UserResource.java
+++ src/main/java/com/basic/spring/basicspring/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.basic.spring.basicspring.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -25,13 +28,25 @@ public class UserResource {
     // Get one user
     @GetMapping("/user/{id}")
     public User retrieveOneUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user==null) {
+            throw new UserNotFoundException("id-"+id);
+        }
+
+        return user;
     }
 
     // Create 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+        // Created 
+        URI location = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(savedUser.getId())
+        .toUri();
+       return ResponseEntity.created(location).build();
     }
 
 }
