fd014cb2807c42d62b7a24441d0ddb75b5b74d88
Anurag <anuragarwalkar@gmail.com>
2021-01-22 19:51:16 +0530

    added internationalization

--- src/main/java/com/basic/spring/basicspring/BasicSpringApplication.java
+++ src/main/java/com/basic/spring/basicspring/BasicSpringApplication.java
@@ -1,13 +1,34 @@
 package com.basic.spring.basicspring;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class BasicSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicSpringApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
--- src/main/java/com/basic/spring/basicspring/helloWorld/BooksController.java
+++ src/main/java/com/basic/spring/basicspring/helloWorld/BooksController.java
@@ -2,14 +2,22 @@ package com.basic.spring.basicspring.helloWorld;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class BooksController {
-    
+
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/books")
     public List<Book> getAllBooks() {
         return Arrays.asList(new Book(1, "life of an hardware", "anurag"));
@@ -24,4 +32,10 @@ public class BooksController {
     public HelloWorldBean getBooksBeanPath(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello world, %s", name));
     }
+
+    @GetMapping(value = "/hello-world-internationalized")
+    public String getMethodName(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
+    }
+
 }
